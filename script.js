28ee7d043f65359260216d3c48b36c6ef5b29c26
Erwin Wu <erwinwu211@gmail.com>
2026-07-01 03:17:38 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".site-nav");
+
+navToggle?.addEventListener("click", () => {
+  const isOpen = nav?.classList.toggle("is-open") ?? false;
+  navToggle.setAttribute("aria-expanded", String(isOpen));
+});
+
+nav?.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("is-open");
+    navToggle?.setAttribute("aria-expanded", "false");
+  });
+});
